8e0b9ca41637e4d098049504ab3c8e13f2427cae
ishijaiswal18 <ishikaj@iitbhilai.ac.in>
2022-03-05 23:02:13 +0530

    Progress in navbar

--- netflix_clone/src/components/Home/Home.jsx
+++ netflix_clone/src/components/Home/Home.jsx
@@ -8,6 +8,25 @@ export const Home = () => {
   return (
     <div className = "home">
       <Navbar />
+      <img 
+        width = "100%"
+        src = "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        alt = "" />
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+        doloremque. Quasi, quidem, quas, dolorum, voluptate, quod, quae quam
+        doloremque quisquam dolores quaerat, voluptatum, quis. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Quisquam, doloremque. Quasi, quidem,
+        quas, dolorum, voluptate, quod, quae quam doloremque quisquam dolores quaerat,
+        voluptatum, quis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quisquam, doloremque. Quasi, quidem, quas, dolorum, voluptate, quod, quae
+        quam doloremque quisquam dolores quaerat, voluptatum, quis. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Quisquam, doloremque. Quasi, quidem,
+        quas, dolorum, voluptate, quod, quae quam doloremque quisquam dolores quaerat,
+        voluptatum, quis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quisquam, doloremque. Quasi, quidem, quas, dolorum, voluptate, quod, quae
+        quam doloremque quisquam dolores quaerat, voluptatum, quis. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Quisquam, doloremque. Quasi, quidem,
     </div>
   )
 }
