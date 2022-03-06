a55a5b35bb2c82f7fec5cc70efad3e7103d965a3
Devvrat <devvrat2001@gmail.com>
2022-03-07 00:22:45 +0530

    slider initial

--- netflix_clone/src/components/List/List.jsx
+++ netflix_clone/src/components/List/List.jsx
@@ -1,9 +1,38 @@
+import { ArrowBackIos, ArrowBackIosOutlined, ArrowForwardIosOutlined } from "@material-ui/icons"
+import ListItem from "../listItem/ListItem"
 import "./list.scss"
 
 export default function List() {
+  // const listRef = useRef()
+  // const handleClick = (direction) =>
+  // {
+  //   let distance = listRef.current.getBoundingClientRect()
+  //   if(direction === "left")
+  //   {
+  //     listRef.current.style.transform = 'translateX(230ps)';
+  //   }
+  //   console.log(distance)
+  // }
   return (
     <div className="list">
-      list
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+      <ArrowBackIosOutlined className="sliderArrow left"  />
+      <div className="container" >
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+        <ListItem/>
+
+      </div>
+      <ArrowForwardIosOutlined className="sliderArrow right" />
+
+      </div>
+
     </div>
   )
 }
