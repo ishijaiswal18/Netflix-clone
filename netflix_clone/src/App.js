aaa9f91864f176115e71c06e1db075c2a118aab0
Devvrat <devvrat2001@gmail.com>
2022-03-08 21:42:11 +0530

    register 1

--- netflix_clone/src/App.js
+++ netflix_clone/src/App.js
@@ -1,12 +1,14 @@
 // import react from 'react';
 import Home from './pages/Home/Home';
 import Watch from './pages/watch/Watch';
+import Register from './pages/register/Register';
 import './app.scss';
 
 function App() {
   return (
-    <Home/>
+    //<Home/>
     // <Watch/>
+    <Register/>
   );
 }
 
